9bad79a74d6eedbfdc155c1ed27a6152e8d0e6c2
Jolene Valenzuela <jolene_valenzuela@dlsu.edu.ph>
2017-12-08 17:05:06 +0800

    Multi-connection support in DBHelper

--- src/model/DBHelper.java
+++ src/model/DBHelper.java
@@ -1,23 +1,44 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.ArrayList;
+import bitronix.tm.BitronixTransactionManager;
+import bitronix.tm.TransactionManagerServices;
 
 public class DBHelper {
 
 	private final String DB_NAME = "advandb_mco3";
-	private final String USERNAME = "root";
+	private final String USERNAME = "ADVANDB";
+	private final String MY_USERNAME = "root";
 	private final String PASSWORD = "1234";
 
+	private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+	private static final String URL_CELINA = "jdbc:mysql://192.168.1.2:3306";
+	private static final String URL_JOLENE = "jdbc:mysql://192.168.1.3:3306";
+	private static final String URL_JILYAN = "jdbc:mysql://192.168.1.4:3306";
+	
 	private DBConnection connection;
+	
+	private BitronixTransactionManager btm;
 
 	public DBHelper() {
 		connection.setConnection(DB_NAME, USERNAME, PASSWORD);
 		this.connection = DBConnection.getInstance();
+		
+		/*---------From CodeProject.com START---------
+		PoolingDataSource mySQLDS = new PoolingDataSource();
+		mySQLDS.setClassName("com.mysql.jdbc.jdbc2.optional.MysqlXADataSource");
+		mySQLDS.setUniqueName("mySqlBtm");
+		mySQLDS.setMaxPoolSize(3);
+		mySQLDS.getDriverProperties().setProperty("databaseName", DB_NAME);
+		mySQLDS.init();
+		---------From CodeProject.com END---------*/
+		
+		btm = TransactionManagerServices.getTransactionManager();
 	}
 
 	private Record toRecord(ResultSet rs) throws SQLException {
@@ -88,31 +109,74 @@ public class DBHelper {
 	// TODO Insert
 	public boolean addRecord(Record record) {
 		String query = 	"INSERT INTO " + Record.TABLE + 
-						" VALUES (?, ?, ?, ?, ?)";
+						" VALUES (?, ?, ?, ?)";
 		
 		try {
-			connection.prepareStatement(query);
+			/*---------TRANSACTION MANAGER STUFF START---------*/
+			btm.begin();
 			
-			connection.setNull(1);
-			connection.setString(2, record.getRegion());
-			connection.setString(3, record.getCountry());
-			connection.setString(4, record.getYear());
-			connection.setDouble(5, record.getValue());
+			Connection c1 = null;
+			Connection c2 = null;
+			Connection c3 = null;
 			
-			connection.executeUpdate();
-			connection.closePreparedStatement();
+			c1 = DriverManager.getConnection(URL_CELINA + "/" + DB_NAME, USERNAME, PASSWORD);
+			c2 = DriverManager.getConnection(URL_JOLENE + "/" + DB_NAME, USERNAME, PASSWORD);
+			c3 = DriverManager.getConnection(URL_JILYAN + "/" + DB_NAME, USERNAME, PASSWORD);
+			
+			PreparedStatement p1 = c1.prepareStatement(query);
+			p1.setString(2, record.getRegion());
+			p1.setString(3, record.getCountry());
+			p1.setString(4, record.getYear());
+			p1.setDouble(5, record.getValue());
+			
+			PreparedStatement p2 = c2.prepareStatement(query);
+			p2.setString(2, record.getRegion());
+			p2.setString(3, record.getCountry());
+			p2.setString(4, record.getYear());
+			p2.setDouble(5, record.getValue());
+			
+			PreparedStatement p3 = c3.prepareStatement(query);
+			p3.setString(2, record.getRegion());
+			p3.setString(3, record.getCountry());
+			p3.setString(4, record.getYear());
+			p3.setDouble(5, record.getValue());
+			
+			p1.executeUpdate();
+			p2.executeUpdate();
+			p3.executeUpdate();
+			
+			//connection.closePreparedStatement();
+			p1.close();
+			p2.close();
+			p3.close();
+			
+			c1.close();
+			c2.close();
+			c3.close();
+			
+			btm.commit();
+			/*---------TRANSACTION MANAGER STUFF END---------*/
 			
 			System.out.println("[RECORD] INSERT SUCCESS!");
 			return true;
 		} catch (SQLException ev) {
 			ev.printStackTrace();
 			System.out.println("[RECORD] INSERT FAILED!");
-		}	
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("[BTM] SYSTEM EXCEPTION");
+			try {
+				btm.rollback();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("[BTM] ROLLBACK FAILURE");
+			}
+		}
 		return false;
 	}
 	
 	// TODO Update
-	public void updateRecord(Record record) {
+	public boolean updateRecord(Record record) {
 		try {
 			String query = 	"UPDATE " + Record.TABLE +
 							" SET " + Record.COL_REGION + " = ?, " +
@@ -121,42 +185,130 @@ public class DBHelper {
 							Record.COL_DATA + " = ? " +
 							" WHERE " + Record.COL_ID + " = ?";
 			
-			connection.prepareStatement(query);
+			/*---------TRANSACTION MANAGER STUFF START---------*/
+			btm.begin();
 			
-			connection.setString(1, record.getRegion());
-			connection.setString(2, record.getCountry());
-			connection.setString(3, record.getYear());
-			connection.setDouble(4, record.getValue());
-			connection.setInt(5, record.getId());
+			Connection c1 = null;
+			Connection c2 = null;
+			Connection c3 = null;
 			
-			connection.executeUpdate();
-			connection.closePreparedStatement();
+			c1 = DriverManager.getConnection(URL_CELINA + "/" + DB_NAME, USERNAME, PASSWORD);
+			c2 = DriverManager.getConnection(URL_JOLENE + "/" + DB_NAME, USERNAME, PASSWORD);
+			c3 = DriverManager.getConnection(URL_JILYAN + "/" + DB_NAME, USERNAME, PASSWORD);
+			
+			PreparedStatement p1 = c1.prepareStatement(query);
+			p1.setString(1, record.getRegion());
+			p1.setString(2, record.getCountry());
+			p1.setString(3, record.getYear());
+			p1.setDouble(4, record.getValue());
+			p1.setInt(5, record.getId());
+			
+			PreparedStatement p2 = c2.prepareStatement(query);
+			p2.setString(1, record.getRegion());
+			p2.setString(2, record.getCountry());
+			p2.setString(3, record.getYear());
+			p2.setDouble(4, record.getValue());
+			p2.setInt(5, record.getId());
+			
+			PreparedStatement p3 = c3.prepareStatement(query);
+			p3.setString(1, record.getRegion());
+			p3.setString(2, record.getCountry());
+			p3.setString(3, record.getYear());
+			p3.setDouble(4, record.getValue());
+			p3.setInt(5, record.getId());
+			
+			p1.executeUpdate();
+			p2.executeUpdate();
+			p3.executeUpdate();
+
+			//connection.closePreparedStatement();
+			p1.close();
+			p2.close();
+			p3.close();
+			
+			c1.close();
+			c2.close();
+			c3.close();
+			
+			btm.commit();
+			/*---------TRANSACTION MANAGER STUFF END---------*/
 			
 			System.out.println("[RECORD] UPDATE SUCCESS! ");
+			return true;
 		} catch (SQLException ev) {
 			ev.printStackTrace();
-			System.out.println("[RECORD] UPDATE FAILED! ");
+			System.out.println("[RECORD] UPDATE FAILED!");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("[BTM] SYSTEM EXCEPTION");
+			try {
+				btm.rollback();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("[BTM] ROLLBACK FAILURE");
+			}
 		}
+		return false;
 	}
 	
 	// TODO Delete
-	public void deleteRecord(int id) {
+	public boolean deleteRecord(int id) {
 		String query = 	"DELETE FROM " + 
 						Record.TABLE +
 						" WHERE " + Record.COL_ID + " = ?";
 		
 		try {
-			connection.prepareStatement(query);
+			/*---------TRANSACTION MANAGER STUFF START---------*/
+			btm.begin();
 			
-			connection.setInt(1, id);
+			Connection c1 = null;
+			Connection c2 = null;
+			Connection c3 = null;
 			
-			connection.executeUpdate();
-			connection.closePreparedStatement();
+			c1 = DriverManager.getConnection(URL_CELINA + "/" + DB_NAME, USERNAME, PASSWORD);
+			c2 = DriverManager.getConnection(URL_JOLENE + "/" + DB_NAME, USERNAME, PASSWORD);
+			c3 = DriverManager.getConnection(URL_JILYAN + "/" + DB_NAME, USERNAME, PASSWORD);
+			
+			PreparedStatement p1 = c1.prepareStatement(query);
+			p1.setInt(1, id);
+			
+			PreparedStatement p2 = c2.prepareStatement(query);
+			p2.setInt(1, id);
+			
+			PreparedStatement p3 = c3.prepareStatement(query);
+			p3.setInt(1, id);
+			
+			p1.executeUpdate();
+			p2.executeUpdate();
+			p3.executeUpdate();
+			
+			//connection.closePreparedStatement();
+			p1.close();
+			p2.close();
+			p3.close();
+			
+			c1.close();
+			c2.close();
+			c3.close();
+			
+			btm.commit();
+			/*---------TRANSACTION MANAGER STUFF END---------*/
 			
 			System.out.println("[RECORD] DELETE SUCCESS!");
+			return true;
 		} catch (SQLException ev) {
-			System.out.println("[RECORD] DELETE FAILED!");
 			ev.printStackTrace();
-		}	
+			System.out.println("[RECORD] DELETE FAILED!");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("[BTM] SYSTEM EXCEPTION");
+			try {
+				btm.rollback();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("[BTM] ROLLBACK FAILURE");
+			}
+		}
+		return false;
 	}
 }
